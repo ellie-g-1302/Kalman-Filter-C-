17919c3661fb853c521b02bad5384213eb42b079
Ellie <161966002+ellie-g-1302@users.noreply.github.com>
2025-09-19 08:46:01 -0400

    Update run_kalman_filter.cpp

--- run_kalman_filter.cpp
+++ run_kalman_filter.cpp
@@ -1,64 +1,81 @@
+#ifndef kalman_filter_h
+#define kalman_filter_h
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include "kalman_filter.h"
 #include "matrix_operations.h"
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <random>
 using namespace std;
-#include <chrono>
-using namespace std::chrono;
 
-// Code for opening CSV from https://medium.com/@ryan_forrester_/reading-csv-files-in-c-how-to-guide-35030eb378ad
-vector<vector<string>> readCSV(const string& filename) {
-  vector<vector<string>> data;
-  ifstream file(filename);
-  
-  if (!file.is_open()) {
-      cerr << "Failed to open file: " << filename << endl;
-      return data;
-  }
+class KalmanFilter {
+  public:
+      vector<vector<float>> A_matrix;
+      vector<vector<float>> H_matrix;
+      vector<vector<float>> Q_matrix;
+      vector<vector<float>> R_matrix;
+      int m;
+      int n; 
 
-  string line;
-  while (getline(file, line)) {
-      vector<string> row;
-      stringstream ss(line);
-      string cell;
+      // Variables
+      vector<vector<float>> predicted_x;
+      vector<vector<float>> measurement;
+      vector<vector<float>> predicted_P;
+      vector<vector<float>> K;
+      vector<vector<float>> estimated_x;
+      vector<vector<float>> estimated_P;
 
-      while (getline(ss, cell, ',')) {
-          row.push_back(cell);
+      KalmanFilter(vector<vector<float>> A, vector<vector<float>> H, 
+          vector<vector<float>> Q, vector<vector<float>> R, vector<vector<float>> P, vector<vector<float>> x){
+          A_matrix = A;
+          H_matrix = H;
+          Q_matrix = Q;
+          R_matrix = R;
+          estimated_x = x;
+          estimated_P = P;
+      }
+      ~KalmanFilter(){}
+
+      void updateData(vector<vector<float>> z0){
+          measurement = z0;
       }
 
-      data.push_back(row);
-  }
+      void predict_state(){
+        predicted_x = matrix_multiplication(A_matrix, estimated_x);
+        }
+        
+      void predict_error_covariance(){
+          vector<vector<float>> result = matrix_multiplication(A_matrix, estimated_P);
+          predicted_P  = matrix_addition(matrix_multiplication(result, transposeMat(A_matrix)), Q_matrix);
+        }
 
-  file.close();
-  return data;
-}
+      
+      void kalman_gain(){
+          vector<vector<float>> result = matrix_multiplication(H_matrix, predicted_P);
+          vector<vector<float>> result_two = transposeMat(H_matrix); 
+          vector<vector<float>> result_three = matrix_multiplication(result, result_two);
+          vector<vector<float>> result_four = matrix_addition(result_three, R_matrix);
+          vector<vector<float>> result_five = matrix_multiplication(predicted_P, result_two);
+          vector<vector<float>> result_six = inverse(result_four.size(), result_four);
+          K = matrix_multiplication(result_five, result_six); 
 
-int main(){
-  ofstream myfileax;
-  myfileax.open("ax_final.csv");
-  vector<vector<float>> A = {{1}};
-  vector<vector<float>> H = {{1}};
-  vector<vector<float>> P = {{1}};
-  vector<vector<float>> R = {{10}};
-  vector<vector<float>> Q = {{10}};
-  vector<vector<float>> x = {{1}};
-  
-  KalmanFilter myObj(A, H, Q, R, P, x);
+        }
+      void estimate(){
+          vector<vector<float>> result = subtractMat(measurement, matrix_multiplication(H_matrix, predicted_x));
+          estimated_x = matrix_addition(predicted_x, matrix_multiplication(K, result));
+        }
+        
+      void compute_error_covariance(){
+          vector<vector<float>> result = matrix_multiplication(K, H_matrix);
+          estimated_P = subtractMat(predicted_P, matrix_multiplication(result, predicted_P));
+      }
+
+      void runKalmanFilter(){
+          predict_state();
+          predict_error_covariance();
+          kalman_gain();
+          estimate();
+          compute_error_covariance();
+      }
+};
 
 
-  vector<vector<string>> ax1 = readCSV("ax1.csv");
-  for (int i = 0; i < ax1[0].size(); i++){
-    vector<vector<float>> z = {{stof(ax1[0][i])}};
-    myObj.updateData(z);
-    myObj.runKalmanFilter();
-    myfileax << myObj.estimated_x[0][0] << endl;
-  }
-
-}
-
-
+    
+#endif /* kalman_filter_h */
