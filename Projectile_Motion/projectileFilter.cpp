5060e7a7cfffcc35e0f39258531926d09a9e2081
Ellie <161966002+ellie-g-1302@users.noreply.github.com>
2026-04-02 21:35:55 -0400

    Create projectileFilter.cpp

---
+++ Projectile_Motion/projectileFilter.cpp
@@ -0,0 +1,35 @@
+using namespace std;
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "matrix_operations.h"
+#include "kalman_filter.h"
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <random>
+
+int main(){
+    float dt = 0.01;
+    float half = 0.5;
+    float f = -1 * half * pow(dt,2);
+
+    vector<vector<float>> A = {{1, dt, f},
+                               {0, 1, -1*dt},
+                               {0, 0, -1}};
+    vector<vector<float>> Q = {{10, 0, 0},
+                               {0, 10, 0},
+                               {0, 0, 25}};
+    vector<vector<float>> P = {{1, 0, 0},
+                               {0, 1, 0},
+                               {0, 0, 1}};
+    vector<vector<float>> H = {{0, 0, 1}};
+    vector<vector<float>> R = {{0.0001}};
+    vector<vector<float>> x = {{500},
+                               {0},
+                               {9.81}};
+    KalmanFilter myObj(A, H, Q, R, P, x);
+    myObj.KF("acceleration.csv", "UWU.csv");
+  
+}
+
