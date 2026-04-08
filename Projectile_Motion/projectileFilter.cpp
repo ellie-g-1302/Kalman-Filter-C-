fb941a9c7c9918388c3de51ad0ea9fbec825a581
Ellie <161966002+ellie-g-1302@users.noreply.github.com>
2026-04-07 21:33:22 -0400

    Update projectileFilter.cpp

--- Projectile_Motion/projectileFilter.cpp
+++ Projectile_Motion/projectileFilter.cpp
@@ -9,27 +9,30 @@ using namespace std;
 #include <string>
 #include <random>
 
+
 int main(){
     float dt = 0.01;
     float half = 0.5;
     float f = -1 * half * pow(dt,2);
+    int N = 1000;
 
     vector<vector<float>> A = {{1, dt, f},
                                {0, 1, -1*dt},
-                               {0, 0, -1}};
+                               {0, 0, 1}};
     vector<vector<float>> Q = {{10, 0, 0},
                                {0, 10, 0},
-                               {0, 0, 25}};
+                               {0, 0, 0.001}};
     vector<vector<float>> P = {{1, 0, 0},
                                {0, 1, 0},
-                               {0, 0, 1}};
+                               {0, 0, 0.001}};
     vector<vector<float>> H = {{0, 0, 1}};
-    vector<vector<float>> R = {{0.0001}};
+    vector<vector<float>> R = {{1000000}};
     vector<vector<float>> x = {{500},
                                {0},
                                {9.81}};
     KalmanFilter myObj(A, H, Q, R, P, x);
-    myObj.KF("acceleration.csv", "UWU.csv");
+    vector<string> names = {"Position", "Velocity", "Acceleration"};
+    myObj.KF("acceleration.csv", "ProjectileData.csv", names, N);
   
 }
 
