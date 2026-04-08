7acaa4637ef577488151e5c62db59b45af0413b6
Ellie <161966002+ellie-g-1302@users.noreply.github.com>
2026-04-07 21:21:01 -0400

    Update projectile.py

--- Projectile_Motion/projectile.py
+++ Projectile_Motion/projectile.py
@@ -3,24 +3,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 t = np.linspace(0, 15, 1000)
-# a = np.full(shape=1000, fill_value=9.81)
-# mu = 0
-# sigma = 0.1
-# noise_shape = a.shape
-# noise = np.random.normal(mu, sigma, size=noise_shape)
-# noisy_signal = noise + a
-# np.savetxt('acceleration.csv', noisy_signal, delimiter=',') 
 a = pd.read_csv("acceleration.csv", header = None)
 filtered = pd.read_csv("ProjectileData.csv", sep="\s+")
-print(filtered["Acce"])
 
-
-
-plt.figure(figsize=(10, 5))
-plt.plot(t, filtered["X"], color="#560088", label=" Filtered Signal")
-plt.xlabel("time(s)")
-plt.ylabel("Position")
-# plt.plot(t, a, c="k",label="True value")
-# plt.plot(t, filtered.iloc[:,0], label = "Filtered Signal",c="#45E235",linestyle="dotted")
-plt.legend()
+fig, ax = plt.subplots(2, 2)
+ax[0,0].plot(t, filtered["Position"], color="#9244FFFF", label=" Filtered Signal")
+ax[0,0].set_xlabel('time(s)')
+ax[0,0].set_ylabel('distance (m)')
+ax[0,0].set_title('Estimated Position')
+ax[0,1].plot(t, filtered["Velocity"], color="#FF5E00", label=" Filtered Signal")
+ax[0,1].set_xlabel('time(s)')
+ax[0,1].set_ylabel('Velocity (m/s)')
+ax[0,1].set_title('Estimated Velocity')
+ax[1,0].plot(t, filtered["Acceleration"], color="#FF007B", label=" Filtered Signal")
+ax[1,0].set_xlabel('time(s)')
+ax[1,0].set_ylabel('Acceleration (m/s^2)')
+ax[1,0].set_title('Estimated Acceleration')
+ax[1,1].plot(t, a, color="#00882B", label=" Filtered Signal")
+ax[1,1].set_xlabel('time(s)')
+ax[1,1].set_ylabel('Acceleration (m/s^2)')
+ax[1,1].set_title('Measured Acceleration')
+plt.tight_layout()
 plt.show()
